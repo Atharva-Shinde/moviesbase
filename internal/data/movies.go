d2d18ca203a3a9cb664b522ba251c9b1473c78b2
Atharva Shinde <atharvashinde179@gmail.com>
2023-12-28 19:01:19 +0530

    chore

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -6,7 +6,8 @@ type Movie struct {
 	ID        int64     `json:"id"`
 	CreatedAt time.Time `json:"-"` //this field is unexported
 	Title     string    `json:"title"`
-	year      int32     `json:"year,omitempty"`           // currently 'year' is unexported, to change this behaviour capitalise 'y'
+	Year      int32     `json:"year,omitempty"`
+	rating    int32     // currently 'rating' is unexported, to change this behaviour capitalise 'r'
 	Runtime   int32     `json:"runtime,omitempty,string"` //json output will be a string
 	Genres    []string  `json:",omitempty"`
 	Version   int32     `json:"version"` // starts from 1 and will increment each time movie info is updated
